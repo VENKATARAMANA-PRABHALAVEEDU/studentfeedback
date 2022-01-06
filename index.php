cedac952302f382f782bd2eddb3ca86c8e093dd8
VENKATA RAMANA PRABHALAVEEDU <65699386+VENKATARAMANA-PRABHALAVEEDU@users.noreply.github.com>
2022-01-06 17:05:17 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,496 @@
+
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+    <title>Online Student Feedback System</title>
+	<?php 
+	include('header-scripts.php');
+	?>
+</head>
+<body>
+    <!-- banner -->
+    <div class="banner">
+        <!-- header -->
+	  <?php 
+	 include('header.php'); 
+	  ?>
+        <!-- //header -->
+        <div class="container">
+            <!-- banner-text -->
+            <div class="banner-text">
+                <div class="slider-info">
+                    <h2>MeRITS <SPAN>ONLINE</SPAN> <span>STUDENT FEEDBACK SYSTEM </span></h2>
+
+					<a href="about.html" class="banner-button btn mt-md-5 mt-4">Know More</a>
+
+
+				</div>
+            </div>
+        </div>
+    </div>
+	 <!-- //banner-text -->
+	 <!--banner form-->
+	<section class="banner_form py-5">
+		<div class="container py-lg-3">
+			<div class="row ban_form">
+				<div class="col-lg-8 bg-white fom-left">
+					<div class="categories_sub cats1">
+						<h2 class="heading-agileinfo">Welcome To <span> MeRITS</span></h2>
+						<p class="vam">Welcome to MeRITS. This website is designed for the students, parents, alumni, recruiters and all other stakeholders of MeRITS. It also provides an outline information about the college to students who aspire to take admission in this college. You are welcome to give any suggestions for improvement of the website so that it serves the stakeholders better.
+
+Thank you for your interest in MeRITS. </p>
+</div>
+				</div>
+				<div class="col-lg-4 reg-fom">
+					<h4 class="text-white text-center">DEPARTMENTS</h4>
+						<form action="#" method="post">
+							<div class="reg-fom-btm mt-5">
+								<div class="fields">
+									<span class="text-white mb-2">Select Departments</span>
+										<select class="form-control">
+											<option>CIVIL ENGINEERING</option>
+											<option>HS Diploma</option>
+											<option>MECHANICAL ENGINEERING</option>
+											<option>ELECTRICAL&ELECTRONICS ENGINEERING</option>
+											<option>ELECTRTICAL COMMUNICATION & ENGINEERING</option>
+											<option>COMPUTER SCIENCE & ENGINEERING</option>
+										</select>
+								</div>
+							</div>
+							<div class="reg-fom-btm mt-3">
+								<div class="fields">
+									<span class="text-white mb-2">Category</span>
+										<select class="form-control">
+											<option>PROGRAMS OFFERED</option>
+											<option>HS </option>
+											<option>INFRASTRUCTURE</option>
+											<option>FACULTY</option>
+											<option>STUDENT ACTIVITIES</option>
+											<option>GALLERY </option>
+									</select>
+								</div>
+							</div>
+							
+							<div class="reg-fom-btm mt-3">
+								<input type="submit" value="Search">
+							</div>
+						</form>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--//banner form-->
+	  <!-- /services -->
+    <section class="banner-bottom-wthree py-lg-5 py-md-5 py-3">
+        <div class="container">
+            <div class="inner-sec-w3ls py-lg-5 py-3">
+               <h3 class="heading-agileinfo text-center">What <span> We do in MeRITS</span></h3>
+                <div class="row middle-grids mt-md-5 pt-4">
+                    <div class="col-lg-4 about-in-w3ls middle-grid-info text-center">
+                        <div class="card">
+                            <div class="card-body">
+                                <i class="fas fa-graduation-cap mb-2"></i>
+                                <h5 class="card-title text-uppercase my-3">
+                                  Professional Study</h5>
+                                <p class="card-text">
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 about-in-w3ls middle-grid-info active text-center">
+                        <div class="card">
+                            <div class="card-body">
+                                <i class="fas fa-book mb-2"></i>
+                                <h5 class="card-title text-uppercase my-3">SummEr Session</h5>
+                                <p class="card-text">
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 about-in-w3ls middle-grid-info text-center">
+                        <div class="card">
+                            <div class="card-body">
+                                <i class="fas fa-globe mb-2"></i>
+                                <h5 class="card-title text-uppercase my-3">Global Education</h5>
+                                <p class="card-text">
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- //services -->
+<!-- choose -->
+	<section class="choose py-5">
+		<div class="container py-md-4 mt-md-3"> 
+			<div class="row inner_w3l_agile_grids-1 ">
+				<div class="col-lg-6 w3layouts_choose_left_grid1">
+					<div class="choose_top">
+						<h4 class="mb-3 mt-3 text_red">Feel Free to Contact Our Agents Directly</h4>
+						<p class="text-red">Nulla pellentesque mi non laoreet eleifend. Integer porttitor mollisar lorem, at molestie arcu pulvinar ut. Proin ac fermentum est. Cras mi ipsum, consectetur ac ipsum in, egestas vestibulum tellus.Proin ac fermentum est. Cras mi ipsum, consectetur ac ipsum in, egestas vestibulum tellus.</p>
+						<a href="services.html" class="btn btn-primary mt-3">Read More</a>
+					</div>
+				</div>
+				<div class="col-lg-6 w3layouts_choose_left_grid2">
+					<div class="row">
+						<div class="col-md-6 w3l_choose_bottom1 mt-3 pt-md-4">
+							<div class="choose_bottom_top">
+                                <i class="fas fa-globe mb-2"></i>
+                                <h5 class="card-title text-uppercase my-3">Education</h5>
+                                <p class="card-text">
+.
+                                </p>
+                            </div>
+						</div>
+						<div class="col-md-6 w3l_choose_bottom2">
+							<div class="choose_bottom_top">
+                                <i class="fas fa-book mb-2"></i>
+                                <h5 class="card-title text-uppercase my-3">Education</h5>
+                                <p class="card-text">.
+                                </p>
+                            </div>
+						</div>
+						<div class="col-md-6 w3l_choose_bottom3 mt-3 pt-md-4">
+							<div class="choose_bottom_top">
+                                <i class="fas fa-graduation-cap mb-2"></i>
+                                <h5 class="card-title text-uppercase my-3">Education</h5>
+                                <p class="card-text">
+                                </p>
+                            </div>
+						</div>
+						<div class="col-md-6 w3l_choose_bottom4">
+							<div class="choose_bottom_top">
+                                <i class="fas fa-globe mb-2"></i>
+                                <h5 class="card-title text-uppercase my-3">Education</h5>
+                                <p class="card-text">
+                                </p>
+                            </div>
+						</div>
+					</div>
+				</div>
+			</div>
+	</div>   
+</section>
+<!-- //choose -->
+	 <!-- /advantage -->
+    <section class="banner-bottom py-lg-5 py-md-5 py-3">
+        <div class="container">
+            <div class="inner-sec py-lg-5 py-3">
+                 <h3 class="heading-agileinfo text-center">Our <span> PROVIDED FEATURES</span></h3>
+               <div class="row middle-grids mt-lg-5 pt-5">
+                   <div class="col-lg-6 advantage-grid-info1">
+                        <div class="advantage_left1 text-center">
+                            <img src="images/P2.jpg" class="img-fluid" alt="">
+                        </div>
+                    </div> 
+					 <div class="col-lg-6 advantage-grid-info pt-md-5">
+                        <div class="advantage_left">
+							<h4 class="mb-2"></h4>
+                            <h3></h3>
+							<p class="mt-2">
+
+<BR>
+>> Fully residential campus spread over 100 acres<BR>
+ >>Tranquil and idyllic atmosphere, very congenial for teaching and learning <BR>
+>> Highly qualified and experienced faculty <BR>
+ >>Well equipped laboratories<BR>
+ >>State of the art computer center <BR>
+ >>Central library with over 20000 volumes and 100 journals & magazines
+</p>
+                        </div>
+                    </div>
+                </div>
+				 <div class="row middle-grids pt-lg-5">
+                    <div class="col-lg-6 advantage-grid-info pt-md-5 mt-md-5">
+                        <div class="advantage_left">
+							<h4 class="mb-2"></h4>
+                            <h3></h3>
+							<p class="mt-2">>>Jawahar Knowledge Centre with Star status <BR>
+>> Separate training and placement cell <BR>
+>> Regular programmes on Personality Development <BR>
+>> Seminars & Group Discussions <BR>
+>>24X7 Internet broadband connectivity <BR>
+ >>Industry-Institute interactions <BR>
+ >>Regular guest lectures and technical talks<BR> 
+>> Fully furnished hostels for boys & girls <BR>
+ >>Campus residence for all teaching and non-teaching staff<BR> 
+ 
+>> Transportation facility 
+ </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 advantage-grid-info1">
+                        <div class="advantage_left2 text-center">
+                            <img src="images/g1.jpg" class="img-fluid" alt="">
+                        </div>
+                    </div> 
+                </div>
+			</div>
+        </div>
+    </section>
+    <!-- //advantage -->
+	<!-- feedback -->
+	<section class="news py-5">
+		<div class="container py-xl-5 py-lg-3">
+			<h3 class="heading-agileinfo text-white text-center">Recent <span> FeedBack About college</span></h3>
+			<div class="owl-carousel owl-theme mt-md-5 pt-4">
+				<div class="item">
+					<div class="feedback-info bg-white py-5 px-4">
+						<h4 class="mb-2">P.venkataRamana
+						</h4>
+						<p>College management is working hard to provide a good environment in the college. College should recruit faculty frequently. </p>
+						<div class="feedback-grids mt-4">
+							<div class="feedback-img">
+								<img src="images/t1.jpg" class="img-fluid rounded-circle" alt="" />
+							</div>
+							<div class="feedback-img-info">
+								<h5>RAMANA</h5>
+							</div>
+							<div class="clearfix"> </div>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="feedback-info bg-white py-5 px-4">
+						<h4 class="mb-2">MAHESWAR REDDY
+						</h4>
+						<p>My college campus , builds,laboratories ,it work shops, mechanic shops and staff was good. Management was good. Hostels and food was good. But it is not city and it is small town. No campus placements was not there. Staff shoul be more experienced and knowledged people shoul be there. Overall my college was good and happy.</p>
+						<div class="feedback-grids mt-4">
+							<div class="feedback-img">
+								<img src="images/te3.jpg" class="img-fluid rounded-circle" alt="" />
+							</div>
+							<div class="feedback-img-info">
+								<h5>Olivia Ruth</h5>
+							</div>
+							<div class="clearfix"> </div>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="feedback-info bg-white py-5 px-4">
+						<h4 class="mb-2">
+						SUKANYA </h4>
+						<p>My college campus , builds,laboratories ,it work shops, mechanic shops and staff was good. Management was good. Hostels and food was good. But it is not city and it is small town. No campus placements was not there. Staff shoul be more experienced and knowledged people shoul be there. Overall my college was good and happy. </p>
+						<div class="feedback-grids mt-4">
+							<div class="feedback-img">
+								<img src="images/te2.jpg" class="img-fluid rounded-circle" alt="" />
+							</div>
+							<div class="feedback-img-info">
+								<h5>Blake Joe</h5>
+							</div>
+							<div class="clearfix"> </div>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="feedback-info bg-white py-5 px-4">
+						<h4 class="mb-2">VINOD
+						</h4>
+						<p>
+My college campus , builds,laboratories ,it work shops, mechanic shops and staff was good. Management was good. Hostels and food was good. But it is not city and it is small town. No campus placements was not there. Staff shoul be more experienced and knowledged people shoul be there. Overall my college was good and happy.</p>
+						<div class="feedback-grids mt-4">
+							<div class="feedback-img">
+								<img src="images/te4.jpg" class="img-fluid rounded-circle" alt="" />
+							</div>
+							<div class="feedback-img-info">
+								<h5>Mary Jane</h5>
+							</div>
+							<div class="clearfix"> </div>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="feedback-info bg-white py-5 px-4">
+						<h4 class="mb-2">CHENNAIAH
+						</h4>
+						<p>Faculty was good and explanation was good. But they also just passed out people are working as staff and also their knowledge is not innovative. </p>
+						<div class="feedback-grids mt-4">
+							<div class="feedback-img">
+								<img src="images/te5.jpg" class="img-fluid rounded-circle" alt="" />
+							</div>
+							<div class="feedback-img-info">
+								<h5>Olivia Ruth</h5>
+							</div>
+							<div class="clearfix"> </div>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="feedback-info bg-white py-5 px-4">
+						<h4 class="mb-2">RAMAU
+						</h4>
+						<p>Faculty was good and explanation was good. But they also just passed out people are working as staff and also their knowledge is not innovative. </p>
+						<div class="feedback-grids mt-4">
+							<div class="feedback-img">
+								<img src="images/te6.jpg" class="img-fluid rounded-circle" alt="" />
+							</div>
+							<div class="feedback-img-info">
+								<h5>Blake Joe</h5>
+							</div>
+							<div class="clearfix"> </div>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="feedback-info bg-white py-5 px-4">
+						<h4 class="mb-2">Tempor incididunt ut labore hytnm
+						</h4>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit sedc dnmo.</p>
+						<div class="feedback-grids mt-4">
+							<div class="feedback-img">
+								<img src="images/te1.jpg" class="img-fluid rounded-circle" alt="" />
+							</div>
+							<div class="feedback-img-info">
+								<h5>Mary Jane</h5>
+							</div>
+							<div class="clearfix"> </div>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="feedback-info bg-white py-5 px-4">
+						<h4 class="mb-2">Tempor incididunt ut labore hytnm
+						</h4>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit sedc dnmo.</p>
+						<div class="feedback-grids mt-4">
+							<div class="feedback-img">
+								<img src="images/te3.jpg" class="img-fluid rounded-circle" alt="" />
+							</div>
+							<div class="feedback-img-info">
+								<h5>Olivia Ruth</h5>
+							</div>
+							<div class="clearfix"> </div>
+						</div>
+					</div>
+				</div>
+				<div class="item">
+					<div class="feedback-info bg-white py-5 px-4">
+						<h4 class="mb-2">Tempor incididunt ut labore hytnm
+						</h4>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit sedc dnmo.</p>
+						<div class="feedback-grids mt-4">
+							<div class="feedback-img">
+								<img src="images/te2.jpg" class="img-fluid rounded-circle" alt="" />
+							</div>
+							<div class="feedback-img-info">
+								<h5>Blake Joe</h5>
+							</div>
+							<div class="clearfix"> </div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- //feedback -->
+	<!-- stats -->
+	<section class="stats py-5">
+		<div class="container py-md-5">
+			<h3 class="heading-agileinfo text-center">Our  <span>Achievements</span></h3>
+			<span class="w3-line black"></span>	
+			<div class="row inner_w3l_agile_grids-1 pt-md-5 mt-4">
+				<div class="col-lg-3 col-sm-6 w3layouts_stats_left w3_counter_grid">
+					<i class="fas fa-trophy mb-2"></i>
+					<p class="counter">2002</p>
+					<h3>Year of Foundation</h3>
+				</div>
+				<div class="col-lg-3 col-sm-6 w3layouts_stats_left w3_counter_grid1">
+					<i class="fas fa-graduation-cap mb-2"></i>
+					<p class="counter">7000</p>
+					<h3>Graduates</h3>
+				</div>
+				<div class="col-lg-3 col-sm-6 w3layouts_stats_left w3_counter_grid2">
+					<i class="fas fa-user mb-2"></i>
+					<p class="counter">100</p>
+					<h3>Certified Staff</h3>
+				</div>
+				<div class="col-lg-3 col-sm-6 w3layouts_stats_left w3_counter_grid2">
+					<i class="fas fa-users mb-2"></i>
+					<p class="counter">800</p>
+					<h3>Students</h3>
+				</div>
+<div class="container py-md-5">
+			
+			<span class="w3-line black"></span>	
+			<div class="row inner_w3l_agile_grids-1 pt-md-5 mt-4">
+				<div class="col-lg-3 col-sm-6 w3layouts_stats_left w3_counter_grid">
+					<i class="fas fa-trophy mb-2"></i>
+					<p class="counter">3</p>
+					<h3>GOLDMEDALS</h3>
+				</div>
+			</div>
+		</div>	
+	</section>
+<!-- //stats -->
+<!--/newsletter-->
+    <footer class="newsletter_right pymd-5 py-4" id="footer">
+        <div class="container">
+            <div class="inner-sec py-md-5 py-3">
+                <div class="row mb-md-4 mb-md-3">
+					<div class="col-lg-3 col-md-6 social-info text-left">
+                       <h3 class="tittle1 foot mb-md-5 mb-4 text-white">Contact us</h3>
+						<p>MeRITS, </p>
+						<p class="my-2"> Udayagiri,nellore (Di)</p>
+						<p class="phone">phone: 8620229031</p>
+					                       <p class="phone">Mail:
+							<a href="mailto:MeRITS@example.com">infos@example.com</a>
+						</p>
+
+                    </div>
+					<div class="col-lg-3 col-md-6 social-info text-left">
+					 <h3 class="tittle1 foot mb-md-5 mb-4 text-white">About Us</h3>
+                       <p> Mekapati Rajamohan Reddy Institute of Technology & Science (MeRITS) was established in the Academic year 2002-2003 with a single - minded aim to provide a perfect platform to the students in the field of Engineering for their academic and overall personality development. The College is affiliated to JNTU Anantapur.
+</p>
+
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!--//newsletter-->
+
+<!-- js -->
+
+<!-- //js -->
+<!-- carousel(for feedback) -->
+	<script src="js/owl.carousel.js"></script>
+	<script>
+		$(document).ready(function () {
+			$('.owl-carousel').owlCarousel({
+				loop: true,
+				margin: 10,
+				responsiveClass: true,
+				responsive: {
+					0: {
+						items: 1,
+						nav: true
+					},
+					600: {
+						items: 2,
+						nav: false
+					},
+					1000: {
+						items: 3,
+						nav: true,
+						loop: false,
+						margin: 20
+					}
+				}
+			})
+		})
+	</script>
+	
+	<!-- //carousel(for feedback) -->
+<!-- stats -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<script src="js/jquery.countup.js"></script>
+		<script>
+			$('.counter').countUp();
+		</script>
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>
